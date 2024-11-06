1b6b3bc13f0ae46cdf73d4dde42899cf06fb3ae3
A-N1khil <nikhilanand1006@gmail.com>
2024-11-06 18:11:07 -0500

    Adding init classes

---
+++ assignment3/collectSubmission.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+CODE=(
+	"cs682/transformer_layers.py"
+	"cs231n/simclr/contrastive_loss.py"
+	"cs231n/simclr/data_utils.py"
+	"cs231n/simclr/utils.py"
+)
+NOTEBOOKS=(
+	"Transformer_Captioning.ipynb"
+	"Self_Supervised_Learning.ipynb"
+	"StyleTransfer.ipynb"
+)
+PDFS=(
+	"Transformer_Captioning.ipynb"
+	"Self_Supervised_Learning.ipynb"
+	"StyleTransfer.ipynb"
+)
+
+FILES=( "${CODE[@]}" "${NOTEBOOKS[@]}" )
+ZIP_FILENAME="a3_code_submission.zip"
+PDF_FILENAME="a3_inline_submission.pdf"
+
+for FILE in "${FILES[@]}"
+do
+	if [ ! -f ${FILE} ]; then
+		echo -e "${C_R}Required file ${FILE} not found, Exiting.${C_E}"
+		exit 0
+	fi
+done
+
+echo -e "### Zipping file ###"
+rm -f ${ZIP_FILENAME}
+zip -q "${ZIP_FILENAME}" -r ${NOTEBOOKS[@]} $(find . -name "*.py") $(find . -name "*.pyx") -x "makepdf.py"
+
+echo -e "### Creating PDFs ###"
+python makepdf.py --notebooks "${PDFS[@]}" --pdf_filename "${PDF_FILENAME}"
+
+echo -e "### Done! Please submit ${ZIP_FILENAME} and ${PDF_FILENAME} to Gradescope. ###"
